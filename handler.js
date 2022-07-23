513ee3bc4fa17f6bbb4f8468969ec9126e12db33
Traptricker <85963782+Traptricker@users.noreply.github.com>
2022-07-23 13:34:43 -0400

    Changes

--- handler.js
+++ handler.js
@@ -4,12 +4,35 @@ class Handler {
     entityIDCounter = 0;
     // Entity Dictionaries
     entities = {};
+    entitiesToAdd = [];
+    entitiesToRemove = [];
     
     update() {
+        // Updates every active entity
         for (let key in this.entities) {
-            let entity = 
+            let entity = this.entities[key];
             entity.update();
         }
+
+        // Adds new entities
+        for (let entity of this.entitiesToAdd) {
+            this.entities[entity.hash] = entity;
+        }
+        this.entitiesToAdd = [];
+
+        // Removes entities
+        for (let entity of this.entitiesToRemove) {
+            delete this.entities[entity.hash];
+        }
+        this.entitiesToRemove = [];
+    }
+    
+    addEntity(entity) {
+        this.entitiesToAdd.push(entity);
+    }
+
+    removeEntity(entity) {
+        this.entitiesToRemove.push(entity);
     }
 
 }

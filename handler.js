101537dc96e8063be98a58a2fdb987695f0d1228
Traptricker <85963782+Traptricker@users.noreply.github.com>
2022-07-23 13:06:14 -0400

    Some initial boiler plate code
    Co-authored-by: Andrew <iBrushC@users.noreply.github.com>
    Co-authored-by: Yikuan Sun <yikuansun@users.noreply.github.com>

---
+++ handler.js
@@ -0,0 +1,15 @@
+class Handler {
+
+    // Values
+    entityIDCounter = 0;
+    // Entity Dictionaries
+    entities = {};
+    
+    update() {
+        for (let key in this.entities) {
+            let entity = 
+            entity.update();
+        }
+    }
+
+}
